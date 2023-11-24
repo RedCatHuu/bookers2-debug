ab90df6c1accc803e516fe402d6ed42b8060d4a9
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-24 08:28:49 +0000

    [add]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,36 @@
 class SearchesController < ApplicationController
+  def search
+    search_word = params[:word]
+    @model = params[:model]
+    how = params[:how]
+    if @model == "User"
+      case how.to_i
+      when 1 then
+        @searches = User.where("name = ?", "#{search_word}")
+      when 2 then
+        @searches = User.where("name LIKE ?", "%#{search_word}")
+      when 3 then
+        @searches = User.where("name LIKE ?", "#{search_word}%")
+      when 4 then
+        @searches = User.where("name LIKE ?", "%#{search_word}%")
+      end
+      
+    else
+      case how.to_i
+      when 1 then
+        @searches = Book.where("title LIKE ? or body LIKE ?", "#{search_word}", "#{search_word}")
+      when 2 then
+        @searches = Book.where("title LIKE ? or body LIKE ?", "%#{search_word}", "%#{search_word}")
+      when 3 then
+        @searches = Book.where("title LIKE ? or body LIKE ?", "#{search_word}%", "#{search_word}%")
+      when 4 then
+        @searches = Book.where("title LIKE ? or body LIKE ?", "%#{search_word}%", "%#{search_word}%")
+      end 
+    end 
+  end 
+  
+  def show
+  end 
+  
+    
 end
