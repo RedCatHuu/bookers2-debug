b69e4c16ed6b969e1c52d62a449e528808429457
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-07 10:25:12 +0000

    [add]tag編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,7 +17,8 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    tag_list = params[:book][:name].split(',')
+    # 変数bookのnameキーを取得し、splitで配列に変換。
+    tag_list = params[:book][:name].split(' ')
     if @book.save
       @book.save_book_tags(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
@@ -34,7 +35,9 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
+    tag_list = params[:book][:name].split(" ")
     if @book.update(book_params)
+      @book.save_book_tags(tag_list)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
--- app/models/book.rb
+++ app/models/book.rb
@@ -15,16 +15,25 @@ class Book < ApplicationRecord
   end 
   
   def save_book_tags(category)
+    # 投稿物にタグが存在していれば、タグの名前を配列として取得。update用
     current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    # 現在のタグから、新規タグを引き、不要なタグを特定。
     old_tags = current_tags - category
+    # 新規タグから現在のタグを引き、新規追加分を特定
     new_tags = category - current_tags
     
     old_tags.each do |old_name|
-      self.tags.delte Tag.find_by(name:old_name)
+      # Tag.find_by(name:old_name)の返り値をdeleteの引数に渡し、中間テーブルのレコードを削除。
+      # 引数のobjectをbook.tagsから消去。
+      # 引数なしのdeleteだと反応しない。
+      # self.tags.delete(name:old_name)だと、引数がTagオブジェクトではなくハッシュであるためにエラーが起こる。
+      self.tags.delete(Tag.find_by(name:old_name))
     end 
     
     new_tags.each do |new_name|
+      # Tag.createにすると同じtagがあっても無視して追加してしまう。tagが重複する。
       tag = Tag.find_or_create_by(name:new_name)
+      # self.tags.push(tag)と同じ意味
       self.tags << tag
     end 
   end 
